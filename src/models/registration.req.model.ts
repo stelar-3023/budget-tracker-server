6db54e95512891c0dfefc310a0ec2abf5393e173
Steve Larsen <slarsen-3@att.net>
2022-02-04 20:06:29 -0600

    adde req and res models

---
+++ src/models/registration.req.model.ts
@@ -0,0 +1,5 @@
+export class RegistrationReqModel {
+  name: string;
+  email: string;
+  password: string;
+}
---
+++ src/models/registration.resp.model.ts
@@ -0,0 +1,4 @@
+export class RegistrationRespModel {
+  status: boolean;
+  message: string;
+}
