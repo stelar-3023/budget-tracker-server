fb117b48932f93f5028ba4c5b30215bee7690a37
Steve Larsen <slarsen-3@att.net>
2022-02-10 18:51:31 -0600

    added jwt strategy

---
+++ src/users/jwt.strategy/jwt.strategy.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express-serve-static-core';
+import { request } from 'http';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      ignoreExpiration: false,
+      secretOrKey: 'secretKey',
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          let secretData = request?.cookies['jwt'];
+          if (!secretData) {
+            return null;
+          }
+          return secretData?.token;
+        },
+      ]),
+    });
+  }
+  async validate(payload: any) {
+    if (!payload) {
+      return new UnauthorizedException();
+    }
+    return payload;
+  }
+}
