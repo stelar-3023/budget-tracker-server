3df95a69d7007fff133450b40c909f287331c1b7
Steve Larsen <slarsen-3@att.net>
2022-03-09 19:09:18 -0600

    added route handlers for update and delete user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,22 @@
-import { Body, Controller, Get, Post, Req, Res, Param,UseGuards, Query, Put, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  Param,
+  UseGuards,
+  Query,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
 import { CurrentUser } from 'src/models/current.user';
 import { RegistrationReqModel } from 'src/models/registration.req.model';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -35,17 +48,19 @@ export class UsersController {
   findAllUsers(@Query('email') email: string) {
     return this.userService.getUserByEmail(email);
   }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.updateUser(id, body);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
+  }
+
+
   
-  // @Put('/:id')
-  // updateUser(@Param('id') id: string, @Body() user: any) {
-  //   return this.userService.updateUser(id, user);
-  // }
-
-  // @Delete('/:id')
-  // deleteUser(@Param('id') id: string) {
-  //   return this.userService.deleteUser(id);
-  // }
-
   @Get('transactions')
   @UseGuards(AuthGuard('jwt'))
   async transactions() {
