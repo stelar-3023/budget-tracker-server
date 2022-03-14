77c278695c4e4a905926899d65b43fe4ed192db2
Steve Larsen <slarsen-3@att.net>
2022-03-13 19:50:27 -0500

    create interceptors

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,9 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
