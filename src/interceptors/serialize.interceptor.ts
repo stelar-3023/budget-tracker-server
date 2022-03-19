d1ef08059fc8f6ccd9ce05e593188776b157b97c
Steve Larsen <slarsen-3@att.net>
2022-03-19 08:53:01 -0500

    added interface & Serialize function to use in the controller

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,3 +7,26 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // run something before a request is handled by the request handler
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        // run something before the response is sent out
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
